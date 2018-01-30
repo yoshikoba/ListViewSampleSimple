8c449863b82612b2725f0b3d46ce98c0ee243a41
yoshikoba <dev.mac.kazukoba@gmail.com>
2018-01-30 11:58:17 +0900

    ・JSONデータをパースの確認

--- app/src/main/java/com/example/kazukoba/listviewsamplesimple/MainActivity.java
+++ app/src/main/java/com/example/kazukoba/listviewsamplesimple/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.kazukoba.listviewsamplesimple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("test",jsonString());
+
+        try {
+            parseJson();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseJson() throws JSONException {
+        JSONObject jsonObject = new JSONObject(jsonString());
+
+        JSONObject error = jsonObject.getJSONArray("data").getJSONObject(1);
+
+        // "code" の値を取得
+        System.out.println(error.getString("shopName"));
+        // -> 215
+
+        // "message" の値を取得
+        System.out.println(error.getString("area"));
+        // -> Bad Authentication data.
+    }
+
+    private String jsonString() {
+        return "{\"data\":[{\"shopName\":\"讃岐うどん　蔵之介\",\"area\":\"東京都豊島区\"},{\"shopName\":\"讃岐うどん いわい\",\"area\":\"東京都北区\"}]}";
     }
 }
